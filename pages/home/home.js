6dcf37b95193753ed599ad148c958ed6999c8f89
5SUPERMAN <53365021+5SUPERMAN@users.noreply.github.com>
2019-09-20 23:47:07 +0800

    初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: '5SUPERMAN',
+    age: 3,
+    students: [
+      { id: 1, name: '小黑', age: 3 },
+      { id: 2, name: '小白', age: 2 },
+      { id: 3, name: '小红', age: 4 },
+      { id: 4, name: '小绿', age: 3 },
+      { id: 5, name: '小蓝', age: 5 }
+    ],
+    counter: 0
   },
+  handleAddition() {
+    // 1.错误做法：可以改变couter内部的值，但是界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData() --- 成功啦!!!
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
