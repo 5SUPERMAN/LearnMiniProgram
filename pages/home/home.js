38869ed57a7ee986869cf7608ccc7258b229e992
5SUPERMAN <53365021+5SUPERMAN@users.noreply.github.com>
2019-09-22 00:48:00 +0800

    生命周期、注册App和Page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,39 @@
 // pages/home/home.js
+
+// 通过 getApp()获取 App()产生的示例对象
+const app = new getApp()
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+
+// 调用 Page，称之为注册一个页面
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  handleGetUserInfo(event) {
+    console.log(event)
+  },
+  click() {
+    console.log('被点击了')
+  },
   data: {
-
+    message: '哈哈哈哈哈',
+    list: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    wx.request({
+      url: 'http://123.207.32.32:8000/api/v1/recommend',
+      success:(res) => {
+        console.log(res)
+        const data = res.data.data.list
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 监听滚动事件
+  onPageScroll(obj) {
+    // console.log(obj)
   }
 })
