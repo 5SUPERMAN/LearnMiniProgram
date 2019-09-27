cb44215358eb2f2a4b1ad95deda1cabbaa047725
5SUPERMAN <53365021+5SUPERMAN@users.noreply.github.com>
2019-09-27 23:51:30 +0800

    部分API展示

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,63 @@
 // pages/home/home.js
 Page({
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中...',
+      duration: 2000,
+      icon: 'loading',
+      mask: true,
+      success: function() {
+        console.log('展示弹窗成功')
+      },
+      fail: function() {
+        console.log('展示弹窗失败')
+      },
+      complete: function() {
+        console.log('点击时直接调用')
+      }
+    })
+  },
+  handleShowModal() {
+    wx.showModal({
+      title: '我是Modal',
+      content: '哈哈哈',
+      // showCancel: false,
+      success: function(res) {
+        console.log(res)
+        if(res.cancel){
+          console.log('点击了取消')
+        }else{
+          console.log('点击了确定')
+        }
+      }
+    })
+  },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    }),
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+    setTimeout(() => {
+      // 必须调用 hideLoading，showLoading才会消失
+      wx.hideLoading()
+    },1000)
+  },
+  handleShowAction()  {
+    wx.showActionSheet({
+      itemList: ['小黑', '小绿', '小黄'],
+      success: function(res) {
+        console.log(res)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 分享设置
+  onShareAppMessage: function(options) {
+    return {
+      title: '哈哈哈',
+      path: '/pages/about/about',
+      // imageUrl: ''
+    }
   }
 })
